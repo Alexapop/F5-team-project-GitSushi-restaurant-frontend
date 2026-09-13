953a1ff8d16128871234a19ce9aef602a3da71b1
Durán <nievesyjonathan23@gmail.com>
2026-09-13 16:01:09 +0200

    feat: build CestaView with the Tu pedido widget

--- src/views/CestaView.vue
+++ src/views/CestaView.vue
@@ -1,3 +1,22 @@
+<script setup>
+import CartSummary from '../components/CartSummary.vue'
+</script>
+
 <template>
-  <div>Cesta</div>
+  <main class="cesta-view">
+    <h1 class="cesta-view__title">Tu cesta</h1>
+    <CartSummary />
+  </main>
 </template>
+
+<style scoped>
+@reference "../style.css";
+
+.cesta-view {
+  @apply max-w-3xl mx-auto px-4 sm:px-6 py-8;
+}
+
+.cesta-view__title {
+  @apply text-2xl font-heading mb-6;
+}
+</style>
